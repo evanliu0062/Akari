e92289f7a20fede2292eade009045d4824c71cb6
Evan Liu <evanliu@ad.unc.edu>
2022-11-14 15:24:29 -0500

    A9 Akari

---
+++ src/main/java/com/comp301/a09akari/model/PuzzleImpl.java
@@ -0,0 +1,56 @@
+package com.comp301.a09akari.model;
+
+import static com.comp301.a09akari.model.CellType.*;
+
+public class PuzzleImpl implements Puzzle {
+  private int[][] board;
+
+  public PuzzleImpl(int[][] board) {
+    this.board = board;
+  }
+
+  @Override
+  public int getWidth() {
+    return board[0].length;
+  }
+
+  @Override
+  public int getHeight() {
+    return board.length;
+  }
+
+  @Override
+  public CellType getCellType(int r, int c) {
+    if (r < 0 || c < 0 || c >= getWidth() || r >= getHeight()) {
+      throw new IndexOutOfBoundsException();
+    }
+
+    switch (board[r][c]) {
+      case 0:
+      case 1:
+      case 2:
+      case 3:
+      case 4:
+        return CLUE;
+      case 5:
+        return WALL;
+      case 6:
+        return CORRIDOR;
+      default:
+        return null;
+    }
+  }
+
+  @Override
+  public int getClue(int r, int c) {
+    if (r < 0 || c < 0 || c >= getWidth() || r >= getHeight()) {
+      throw new IndexOutOfBoundsException();
+    }
+
+    if (getCellType(r, c) == CLUE) {
+      return board[r][c];
+    } else {
+      throw new IllegalArgumentException();
+    }
+  }
+}
